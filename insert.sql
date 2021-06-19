8d6197a6478f599bee318a2623ca509bd73590ff
Sachin Thomas <sachin.t2001@gmail.com>
2021-06-19 22:26:25 +0400

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,48 +1,57 @@
--- inserting values into table: publisher
+--INSERTING VALUES INTO TABLE-PUBLISHER
 
-insert into publisher (name, country) values ("PHI","India");
-insert into publisher (name, country) values ("Harper","USA");
-insert into publisher (name, country) values ("GCP","USA");
-insert into publisher (name, country) values ("Avery","USA");
-insert into publisher (name, country) values ("Del Rey","UK");
-insert into publisher (name, country) values ("Vintage","UK");
+INSERT INTO publisher(name,country) VALUES("PHI","India");
+INSERT INTO publisher(name,country) VALUES("Harper","USA");
+INSERT INTO publisher(name,country) VALUES("GCP","USA");
+INSERT INTO publisher(name,country) VALUES("Avery","USA");
+INSERT INTO publisher(name,country) VALUES("Del Rey","UK");
+INSERT INTO publisher(name,country) VALUES("Vintage","UK");
 
--- inserting values into table: books
-insert into books (title, publisher) values ("The C Programming Language",1);
-insert into books (title, publisher) values ("The Go Programming Language",1);
-insert into books (title, publisher) values ("The UNIX Programming Environment",1);
-insert into books (title, publisher) values ("Cryptonomicon",2);
-insert into books (title, publisher) values ("Deep Work",3);
-insert into books (title, publisher) values ("Atomic Habits",4);
-insert into books (title, publisher) values ("The City and The City",5);
-insert into books (title, publisher) values ("The Great War for Civilisation",6);
 
--- inserting values into table: subjects
-insert into subjects (name) values ("C");
-insert into subjects (name) values ("UNIX");
-insert into subjects (name) values ("Technology");
-insert into subjects (name) values ("Go");
-insert into subjects (name) values ("Science Fiction");
-insert into subjects (name) values ("Productivity");
-insert into subjects (name) values ("Psychology");
-insert into subjects (name) values ("Politics");
-insert into subjects (name) values ("History");
 
--- inserting values into table: books_subjects
-insert into books_subjects(book, subject) values (1,1);
-insert into books_subjects(book, subject) values (1,2);
-insert into books_subjects(book, subject) values (1,3);
-insert into books_subjects(book, subject) values (2,3);
-insert into books_subjects(book, subject) values (2,4);
-insert into books_subjects(book, subject) values (3,2);
-insert into books_subjects(book, subject) values (3,3);
-insert into books_subjects(book, subject) values (4,3);
-insert into books_subjects(book, subject) values (4,5);
-insert into books_subjects(book, subject) values (5,3);
-insert into books_subjects(book, subject) values (5,6);
-insert into books_subjects(book, subject) values (6,6);
-insert into books_subjects(book, subject) values (6,7);
-insert into books_subjects(book, subject) values (7,5);
-insert into books_subjects(book, subject) values (7,8);
-insert into books_subjects(book, subject) values (8,8);
-insert into books_subjects(book, subject) values (8,9);
+--INSERTING VALUES INTO TABLE-BOOKS
+
+INSERT INTO books(title,publisher) VALUES("The C Programming Language",1);
+INSERT INTO books(title,publisher) VALUES("The Go Programming Language",1);
+INSERT INTO books(title,publisher) VALUES("The UNIX Programming Environment",1);
+INSERT INTO books(title,publisher) VALUES("Cryptonomicon",2);
+INSERT INTO books(title,publisher) VALUES("Deep Work",3);
+INSERT INTO books(title,publisher) VALUES("Atomic Habits",4);
+INSERT INTO books(title,publisher) VALUES("The City and The City",5);
+INSERT INTO books(title,publisher) VALUES("The Great War for Civilisation",6);
+
+
+
+--INSERTING VALUES INTO TABLE-SUBJECTS
+
+INSERT INTO subjects(name) VALUES("C");
+INSERT INTO subjects(name) VALUES("UNIX");
+INSERT INTO subjects(name) VALUES("Technology");
+INSERT INTO subjects(name) VALUES("Go");
+INSERT INTO subjects(name) VALUES("Science Fiction");
+INSERT INTO subjects(name) VALUES("Productivity");
+INSERT INTO subjects(name) VALUES("Psychology");
+INSERT INTO subjects(name) VALUES("Politics");
+INSERT INTO subjects(name) VALUES("History");
+
+
+
+--INSERTING VALUES INTO TABLE-BOOKS_SUBJECTS
+
+INSERT INTO books_subjects(book,subject) VALUES(1,1);
+INSERT INTO books_subjects(book,subject) VALUES(1,2);
+INSERT INTO books_subjects(book,subject) VALUES(1,3);
+INSERT INTO books_subjects(book,subject) VALUES(2,4);
+INSERT INTO books_subjects(book,subject) VALUES(2,3);
+INSERT INTO books_subjects(book,subject) VALUES(3,2);
+INSERT INTO books_subjects(book,subject) VALUES(3,3);
+INSERT INTO books_subjects(book,subject) VALUES(4,3);
+INSERT INTO books_subjects(book,subject) VALUES(4,5);
+INSERT INTO books_subjects(book,subject) VALUES(5,3);
+INSERT INTO books_subjects(book,subject) VALUES(5,6);
+INSERT INTO books_subjects(book,subject) VALUES(6,6);
+INSERT INTO books_subjects(book,subject) VALUES(6,7);
+INSERT INTO books_subjects(book,subject) VALUES(7,5);
+INSERT INTO books_subjects(book,subject) VALUES(7,8);
+INSERT INTO books_subjects(book,subject) VALUES(8,8);
+INSERT INTO books_subjects(book,subject) VALUES(8,9);
