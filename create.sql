c75c7eaaf37c3bbc962de087b16ec99c6ed4af59
Sachin Thomas <sachin.t2001@gmail.com>
2021-06-19 22:23:55 +0400

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,7 @@
-create table publisher(id integer primary key,name text,country text);
-create table books(id integer primary key,title text,publisher text,foreign key(publisher) references publisher(id));
-create table subjects(id integer primary key,name text);
-create table books_subjects(book text,subject text,foreign key (book) references books(id),foreign key (subject) references subjects(id));
+CREATE TABLE publisher(	id integer primary key,	name text,	country text);
+
+CREATE TABLE books(	id integer primary key,	title text,	publisher integer references publisher(id));
+
+CREATE TABLE subjects(	id integer primary key,	name text);
+
+CREATE TABLE books_subjects(book integer references books(id),	subject integer references subjects(id));
